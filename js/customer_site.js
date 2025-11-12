5c232fd2183c242f6e28622e70c6eb7fc3b6bd0a
chason8090 <155939759+chason8090@users.noreply.github.com>
2025-11-12 17:25:39 +0800

    Add new customer sites with APIs and names
    Add new customer sites with APIs and names

--- js/customer_site.js
+++ js/customer_site.js
@@ -1,7 +1,19 @@
 const CUSTOMER_SITES = {
-    qiqi: {
-        api: 'https://www.qiqidys.com/api.php/provide/vod',
-        name: '七七资源',
+    jisu: {
+        api: 'https://jszyapi.com/api.php/provide/vod/at/json',
+        name: '极速资源',
+    },        
+    maotai: {
+        api: 'https://caiji.maotaizy.cc/api.php/provide/vod/at/josn/',
+        name: '茅台资源',
+    },
+    jinying: {
+        api: ' https://jyzyapi.com/provide/vod/from/jinyingyun/at/json',
+        name: '金鹰资源',
+    },
+     uku: {
+        api: 'https://api.ukuapi88.com/api.php/provide/vod/',
+        name: 'U酷资源',
     }
 };
 
